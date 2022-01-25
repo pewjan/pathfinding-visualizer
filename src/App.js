7381b3f1bed4dfd010d501ffd0a5c181de137ca0
BabyRin <flamingrin17@gmail.com>
2022-01-25 14:28:55 -0600

    added a non functioning grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import "./App.css";
 
 function App() {
+  const gridArr = [];
+  const displayGrid = () => {
+    for (let row = 0; row < 10; row++) {
+      gridArr.push([]);
+      for (let col = 0; col < 20; col++) {
+        gridArr[row].push(<div className={`column`}>&nbsp;</div>);
+      }
+    }
+  };
+  displayGrid();
   return (
     <div className="App">
-      <navbar className="navbar">
+      <nav className="navbar">
         <h1 className="logo">PathFinder</h1>
         <ul className="rightRow">
-          <li>Vizualize</li>
+          <li className="vizualizebtn">Vizualize</li>
         </ul>
-      </navbar>
+      </nav>
       <div className="main">
-        <div className="grid"></div>
+        <h1>Click on two points to find the path</h1>
+        <div className="grid">
+          {gridArr.map((grid, index) => {
+            return (
+              <div className={`row${index}`}>
+                {grid.map((item) => {
+                  return item;
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
       <footer className="footer"></footer>
     </div>
