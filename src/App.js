660829d3cff513f33ada1ca139fa11ced8c17f24
BabyRin <flamingrin17@gmail.com>
2022-01-25 12:25:36 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <navbar className="navbar">
+        <h1 className="logo">PathFinder</h1>
+        <ul className="rightRow">
+          <li>Vizualize</li>
+        </ul>
+      </navbar>
+      <div className="main">
+        <div className="grid"></div>
+      </div>
+      <footer className="footer"></footer>
     </div>
   );
 }
