e1b3b46678d74eaba7ab57ef7a9aafb735ba3408
BabyRin <flamingrin17@gmail.com>
2022-01-31 01:39:14 -0600

    sub menu for algo

--- src/App.js
+++ src/App.js
@@ -6,46 +6,36 @@ function App() {
   const handleMenu = (e) => {
     if (e.target.innerHTML === "Algorithms ▼") {
       setOpenedAlgo(!openedAlgo);
+      console.log(e.target.innerHTML);
     }
   };
-  const gridArr = [];
-  const displayGrid = () => {
-    for (let row = 0; row < 10; row++) {
-      gridArr.push([]);
-      for (let col = 0; col < 20; col++) {
-        gridArr[row].push(<div className={`column`}>&nbsp;</div>);
-      }
-    }
-  };
-  displayGrid();
   return (
     <div className="App">
       <nav className="navbar">
         <h1 className="logo">PathFinder</h1>
         <ul className="rightRow">
-          <li
-            onClick={(e) => {
-              handleMenu(e);
-            }}
-          >
-            Algorithms ▼
+          <li className="algorithm">
+            <p
+              onClick={(e) => {
+                handleMenu(e);
+              }}
+              className={`algorithms ${openedAlgo && "clicked"} `}
+            >
+              Algorithms ▼
+            </p>
+            {openedAlgo && (
+              <div className="algo">
+                <p>A* Search</p>
+                <p>Dijkstra's Algorithm</p>
+                <p>Breadth-first Search</p>
+              </div>
+            )}
           </li>
           <li className="vizualizebtn">Vizualize</li>
         </ul>
       </nav>
       <div className="main">
         <h1>Click on two points to find the path</h1>
-        <div className="grid">
-          {gridArr.map((grid, index) => {
-            return (
-              <div className={`row${index}`}>
-                {grid.map((item) => {
-                  return item;
-                })}
-              </div>
-            );
-          })}
-        </div>
       </div>
       <footer className="footer">
         <div className="items">
